c11e989244530369572393871c84a7184184461d
fogge <frederickscubi@gmail.com>
2026-04-08 22:00:41 +0300

    WP 04.08.26 What I've done: 1)I already moved the logic downloading and downloading with secure

--- internal/Service/Handlers/ServiceDownloadEncrypt.go
+++ internal/Service/Handlers/ServiceDownloadEncrypt.go
@@ -11,11 +11,7 @@ import (
 	"log/slog"
 	"net/http"
 	"strconv"
-	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -35,65 +31,21 @@ func (sa *HandlerPackCollect) DownloadEncrypt(w http.ResponseWriter, ctxs contex
 
 	Reader, writer := io.Pipe()
 
-	err = sa.downloadFileToClient(w, ctxs, name, writer, aesKey, realFileName, Reader)
+	g, ctx := errgroup.WithContext(ctxs)
+
+	Body, FileLength, err := sa.S3.S3Download.DownloadSecure(ctxs, name)
 	if err != nil {
 		return err
 	}
-
-	slog.Info("Func Delete start in download encrypt")
-
-	err = sa.RedisControlling.Deleter.DeleteFileInfo(name, ctxs)
-	if err != nil {
-		return err
-	}
-	err = sa.S3.Deleter.DeleteFileFromS3(name, ctxs)
-	if err != nil {
-		return err
-	}
-
-	slog.Info("Func Delete ends in download encrypt  ")
-
-	return nil
-}
-
-func (sa *HandlerPackCollect) downloadFileToClient(w http.ResponseWriter, ctx context.Context, name string, writer *io.PipeWriter, aesKey []byte, realFileName string, Reader *io.PipeReader) error {
-
-	downloader := *s3.New(session.Must(session.NewSession()))
-
-	o, err := downloader.GetObjectWithContext(ctx, &s3.GetObjectInput{
-		Bucket:      aws.String(Bucket),
-		IfNoneMatch: aws.String(""),
-		Key:         aws.String(name),
-	})
-
 	defer func(Body io.ReadCloser) {
-		err = Body.Close()
+		err := Body.Close()
 		if err != nil {
-			slog.Error("Error closing body", "Err", err)
+			slog.Error("Error closing body", err)
 			return
-
 		}
-	}(o.Body)
+		return
+	}(Body)
 
-	switch {
-	case strings.Contains(fmt.Sprint(err), "NoSuchKey"):
-		slog.Info("File was used")
-		return errors.New("file was used")
-
-	case errors.Is(err, context.DeadlineExceeded):
-		slog.Error("Time was exceeded")
-		return errors.New("time was exceeded")
-	case errors.Is(err, context.Canceled):
-		slog.Info("a user has been cancelled download ")
-		return errors.New("a user has been canceled download ")
-
-	}
-	if err != nil {
-		slog.Error("ServiceDownload:", "Error", err.Error())
-		return err
-	}
-
-	g, ctx := errgroup.WithContext(ctx)
 	g.Go(func() error {
 		defer func(writer *io.PipeWriter) {
 			err := writer.Close()
@@ -102,7 +54,7 @@ func (sa *HandlerPackCollect) downloadFileToClient(w http.ResponseWriter, ctx co
 				return
 			}
 		}(writer)
-		err = DecryptFile(aesKey, o, writer)
+		err = DecryptFile(aesKey, Body, writer, ctx)
 		if err != nil {
 			err := writer.CloseWithError(err)
 			if err != nil {
@@ -117,9 +69,14 @@ func (sa *HandlerPackCollect) downloadFileToClient(w http.ResponseWriter, ctx co
 	FormatFile := sa.FileInfo.FileManaging.FindFormatOfFile(realFileName)
 	w.Header().Set("Content-Type", FormatFile)
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename= %v", realFileName))
-	w.Header().Set("Content-Length", strconv.FormatInt(*o.ContentLength-aes.BlockSize, 10))
+	w.Header().Set("Content-Length", strconv.FormatInt(FileLength-aes.BlockSize, 10))
 
 	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			return ctxs.Err()
+		default:
+		}
 		if _, err = io.Copy(w, Reader); err != nil {
 			slog.Error("Err In file Service Downloader EncryptFile", "err", err)
 			return errors.New("connect close")
@@ -130,20 +87,85 @@ func (sa *HandlerPackCollect) downloadFileToClient(w http.ResponseWriter, ctx co
 	if err := g.Wait(); err != nil {
 		return err
 	}
-
+	slog.Info("Start deleting file")
+	err = sa.RedisControlling.Deleter.DeleteFileInfo(name, ctxs)
+	if err != nil {
+		return err
+	}
+	err = sa.S3.Deleter.DeleteFileFromS3(name, ctxs)
+	if err != nil {
+		return err
+	}
+	slog.Info("Deleted file from S3")
 	return nil
 }
 
-func DecryptFile(AesKey []byte, o *s3.GetObjectOutput, writer *io.PipeWriter) error {
+//func (sa *HandlerPackCollect) downloadFileToClient(w http.ResponseWriter, ctx context.Context, name string, writer *io.PipeWriter, aesKey []byte, realFileName string, Reader *io.PipeReader) error {
+//	OldS3Connect, err := Helpers.Inzelire()
+//	if err != nil {
+//		slog.Error("Error connect to s3 old ", "Error", err)
+//		return
+//	}
+//	downloader := *s3.New(OldS3Connect)
+//
+//	o, err := downloader.GetObjectWithContext(ctx, &s3.GetObjectInput{
+//		Bucket:      aws.String(Bucket),
+//		IfNoneMatch: aws.String(""),
+//		Key:         aws.String(name),
+//	})
+//
+//	defer func(Body io.ReadCloser) {
+//		err = Body.Close()
+//		if err != nil {
+//			slog.Error("Error closing body", "Err", err)
+//			return
+//
+//		}
+//	}(o.Body)
+//
+//	switch {
+//	case strings.Contains(fmt.Sprint(err), "NoSuchKey"):
+//		slog.Info("File was used")
+//		return errors.New("file was used")
+//
+//	case errors.Is(err, context.DeadlineExceeded):
+//		slog.Error("Time was exceeded")
+//		return errors.New("time was exceeded")
+//	case errors.Is(err, context.Canceled):
+//		slog.Info("a user has been cancelled download ")
+//		return errors.New("a user has been canceled download ")
+//
+//	}
+//	if err != nil {
+//		slog.Error("ServiceDownload:", "Error", err.Error())
+//		return err
+//	}
+//
+//	g, ctx := errgroup.WithContext(ctx)
+//	g.Go(func() error {
+//		defer func(writer *io.PipeWriter) {
+//			err := writer.Close()
+//			if err != nil {
+//				slog.Error("Writer can't close", "Err", err)
+//				return
+//			}
+//		}(writer)
+//		err = DecryptFile(aesKey, o.Body, writer, ctx)
+//		if err != nil {
+//			err := writer.CloseWithError(err)
+//			if err != nil {
+//				slog.Error("Error", "err", err)
+//				return nil
+//			}
+//
+//		}
+//		return nil
+//	})
+//
+//	return nil
+//}
 
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			slog.Error("Error close s3 connection ", "err", err)
-			return
-
-		}
-	}(o.Body)
+func DecryptFile(AesKey []byte, o io.ReadCloser, writer *io.PipeWriter, ctx context.Context) error {
 
 	block, err := aes.NewCipher(AesKey)
 	if err != nil {
@@ -152,7 +174,7 @@ func DecryptFile(AesKey []byte, o *s3.GetObjectOutput, writer *io.PipeWriter) er
 	}
 
 	nonce := make([]byte, aes.BlockSize)
-	_, err = io.ReadFull(o.Body, nonce)
+	_, err = io.ReadFull(o, nonce)
 	if err != nil {
 		slog.Error("Error in read", "Error", err.Error())
 		return err
@@ -162,8 +184,11 @@ func DecryptFile(AesKey []byte, o *s3.GetObjectOutput, writer *io.PipeWriter) er
 
 	stream := cipher.NewCTR(block, nonce)
 
-	file := bufio.NewReader(o.Body)
+	file := bufio.NewReader(o)
 	for {
+		if ctx.Err() != nil {
+			return errors.New("context canceled")
+		}
 		n, err := file.Read(plaintext)
 		if err != nil && err != io.EOF {
 			slog.Error("Error in file", "Error", err.Error())
