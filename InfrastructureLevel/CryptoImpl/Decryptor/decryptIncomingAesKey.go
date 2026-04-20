7a72a1f7e53f8d29a20dd47b5fd15f654557c275
fogge <frederickscubi@gmail.com>
2026-04-20 20:21:08 +0300

    SV - 03-17-26
    The stateful version

--- InfrastructureLevel/CryptoImpl/Decryptor/decryptIncomingAesKey.go
+++ InfrastructureLevel/CryptoImpl/Decryptor/decryptIncomingAesKey.go
@@ -14,19 +14,19 @@ func (g Decrypting) GrpcDecrypterAesKey(AesKey []byte) ([]byte, error) {
 
 	KeyInBytes, err := hex.DecodeString(Dto.Keys.MasterServerKey)
 	if err != nil {
-		slog.Error("Error decoding master server key", "error", err.Error())
+		slog.Error("Error decoding master server key", err.Error())
 		return nil, err
 	}
 
 	KeyRsaType, err := x509.ParsePKCS1PrivateKey(KeyInBytes)
 	if err != nil {
-		slog.Error("Error parsing the master server key", "Func: DecryptIncomingAesKey ", "error", err.Error())
+		slog.Error("Error parsing the master server key", err.Error())
 		return nil, err
 	}
 
 	DecryptedAesKey, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, KeyRsaType, AesKey, nil)
 	if err != nil {
-		slog.Error("Error decrypting master server key RSA", "error", err.Error())
+		slog.Error("Error decrypting master server key RSA", err.Error())
 		return nil, err
 	}
 	return DecryptedAesKey, nil
